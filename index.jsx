27803846b30f9acf2d197a69497d86c588ea0b82
ShusevA <60173266+ShusevA@users.noreply.github.com>
2020-09-11 16:57:13 +0300

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,11 @@
+import "./My.css"
+
+import React from "react"
+import ReactDOM from "react-dom";
+
+import Project from "./Project";
+
+ReactDOM.render(
+    <Project/>,
+    document.getElementById("show")
+);
